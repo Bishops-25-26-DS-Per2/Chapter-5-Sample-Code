8ce495b68e33975af95d085ce99ce059ec39a4c3
TBSDrJ (Marcus Jaiclin) <marcus.jaiclin@bishops.com>
2025-12-08 12:34:00 -0800

    Add some more detail to Use Case #3. Add class attribute and class method.

--- properties_case_3.py
+++ properties_case_3.py
@@ -1,34 +1,11 @@
-class SimpleClass:
-    def __init__(self, x: int, y: int, s: str):
-        # To make self.x read-only, use self._x to store its value.
-        self._x = x
-        self.y = y
-        self.s = s
-    def __str__(self) -> str:
-        return f"SimpleClass({self.x}, {self.y}, '{self.s}')"
-    def __repr__(self) -> str:
-        return self.__str__()
-    # Use Case #1 for @property: Make self.x read-only.
-    # So the method x takes the place of the attribute x.
-    # The property decorator makes the method accessible using 
-    #   attribute notation.
-    @property
-    def x(self):
-        return self._x
-    # Use Case #2 for @property: Make a computed attribute.
-    # So this allows you to create something that behaves like an attribute
-    #   that isn't actually an attribute.
-    # As it is, this attribute is read-only also.
-    # So, this allows the code to behave like the way the human would think 
-    #   about the code -- this is really an attribute of the object, so let 
-    #   the code behave like an attribute.
-    @property
-    def sum(self):
-        return self.x + self.y
-
 class Circle:
+    # This is a *class* attribute. Python will also make a copy of
+    #   each class attribute into each instance.
+    PI = 3.14159265358979
+
     def __init__(self, radius: float):
-        self.radius = float(radius)
+        self.radius = radius
+
     # Essentially a computed attribute; self.r matches self.radius
     @property
     def r(self) -> float:
@@ -39,6 +16,18 @@ class Circle:
     def radius(self) -> float:
         return self._radius
 
+    # Computed attribute that gives an alternate route to the data
+    @property
+    def circumference(self) -> float:
+        return self.radius * self.PI * 2
+
+    # By using self.radius instead of self._radius, we call the setter
+    #   for radius, which means that this will call the validation code
+    #   that we wrote for the setter method.
+    @r.setter
+    def r(self, new_r: float) -> None:
+        self.radius = new_r
+
     # This allows us to change the value of radius
     # Notice: the method has the same name as the one above!! Weird.
     # So now you can be sure that you have a non-negative float value for
@@ -48,12 +37,30 @@ class Circle:
         if new_radius < 0:
             msg = "Dude. Don't make a negative radius.  Bad form."
             raise ValueError(msg)
-        self._radius = new_radius
+        self._radius = float(new_radius)
+
+    @circumference.setter
+    def circumference(self, new_circumference: float) -> None:
+        self.radius = new_circumference / (2*self.PI)
+
+    @classmethod
+    def from_circumference(cls, circumference: float) -> 'Circle':
+        """Class methods are often used as alternate constructors.
+        
+        Notice that cls refers to the class."""
+        radius = circumference / (2*cls.PI)
+        return Circle(radius)
+
+    # Can also do @staticmethod if you'd like.
 
 def main():
     c1 = Circle(5)
     print(c1.radius)
     print(c1.r)
+    c1.circumference = 42
+    print(c1.r)
+    c2 = Circle.from_circumference(811)
+    print(c2.radius)
 
 if __name__ == "__main__":
     main()
